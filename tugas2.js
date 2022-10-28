16aecf60d1e0d6d84470bdb0e4ced5d4ed85a3b4
Amin Kamal <aminkamal355@gmail.com>
2022-10-28 21:34:33 +0800

    Tugas 2 JavaScript
    Saya mengumpulkan tugas JavaScript.

---
+++ tugas2.js
@@ -0,0 +1,53 @@
+// tipe data number
+
+
+var number = 22;
+var desimal = 56.6;
+var  mines = -99;
+
+
+
+
+
+
+console.log(number)
+console.log(desimal)
+console.log(mines)
+
+// tipe datas string
+
+
+var amin = "Amin Kamal"
+
+console.log(amin)
+
+
+// tipe data Boolean 
+
+var amin = false;
+
+var kamal = 299;
+
+if(kamal>200)
+kamal = true;
+else
+kamal = false;
+
+console.log(kamal);
+
+// tipe data null
+
+
+
+var dodo = "Ronaldo";
+dodo = null;
+
+console.log(dodo)
+
+
+// tipe data Array
+
+
+var angka = [4,45,99,"Amin Kamal"]
+
+console.log(angka[3])
